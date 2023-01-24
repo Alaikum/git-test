1c30265df20f043ae6777fd93c5a992e651cbecc
Alaikum <carangiadamo@gmail.com>
2023-01-24 17:25:08 +0100

    first commit

---
+++ src/org/lessons/java/Pausa.java
@@ -0,0 +1,14 @@
+package org.lessons.java;
+import java.time.LocalDateTime; // Import the LocalDateTime class
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
+public class Pausa {
+  public static void main(String[] args) {
+    LocalDateTime myDateObj = LocalDateTime.now();
+    System.out.println("Non Formattata: " + myDateObj);
+    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    String formattedDate = myDateObj.format(myFormatObj);
+    System.out.println("Data Formattata: " + formattedDate);
+  }
+}
